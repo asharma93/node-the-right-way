7746434bb3d046b61db5035440b0b93ef606830c
Amit <a.k.sharma1@outlook.com>
2015-05-25 12:55:06 +0100

    Update

--- messaging/zmq-watcher-pub.js
+++ messaging/zmq-watcher-pub.js
@@ -1,9 +1,15 @@
 "use-strict";
 
+/*
+    ZeroMQ is a robust networking package that we will use in this section.
+    This is our publisher file that waits for a subscriber to connect. On connecting,
+    We send a blob of JSON down the wire.
+*/
+
 const
     fs = require("fs"),
     zmq = require("zmq"),
-// Create publisher endpoint
+    // Create publisher endpoint
     publisher = zmq.socket("pub"),
     filename = process.argv[2];
 
